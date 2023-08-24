baf77e221348a9f28e5e5a49d5a7f6d056bc7e65
Swapnendu003 <126004998+Swapnendu003@users.noreply.github.com>
2023-08-25 03:02:06 +0530

    added the about me section

--- script.js
+++ script.js
@@ -1,30 +1,9 @@
-const profileImage = document.getElementById('profile-image');
-const options = document.querySelector('.options');
-const callOption = document.querySelector('.call-option');
-const messageOption = document.querySelector('.message-option');
+document.addEventListener("DOMContentLoaded", function () {
+  const navToggle = document.querySelector(".nav-toggle");
+  const navLinks = document.querySelector(".nav-links");
 
-profileImage.addEventListener('click', () => {
-  profileImage.classList.toggle('spin');
-  options.classList.toggle('show-options');
-});
-
-callOption.addEventListener('click', () => {
-  window.location.href = 'tel:8345952856';
-  console.log('Call option clicked');
-});
-
-messageOption.addEventListener('click', () => {
-   window.location.href = 'sms:8345952856';
-  console.log('Message option clicked');
-});
-
-const arrowButtons = document.querySelectorAll('.arrow-button');
-
-arrowButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const parentLi = button.closest('li');
-    const fullContent = parentLi.querySelector('.full-content');
-    fullContent.classList.toggle('show-full-content');
+  navToggle.addEventListener("click", function () {
+    console.log("Toggle button clicked");
+    navLinks.classList.toggle("open");
   });
 });
-
